8e487681643c4daa78756daea7a812a95bbc6805
Liliia Bilous <liliyabilous.email@gmail.com>
2024-05-07 15:59:47 +0300

    added swiper for project section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import "./styles/scss/main.scss";
 import "./app";
+
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+
+new Swiper(".projects__swiper", {
+  modules: [Navigation],
+  slidesPerView: 3,
+  spaceBetween: 30,
+  speed: 800,
+  navigation: {
+    nextEl: ".arrow-right",
+    prevEl: ".arrow-left",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
