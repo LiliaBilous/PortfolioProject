c5f0221a3d11715d2a83fe7e2f45b5fc10dbbca4
Liliia Bilous <liliyabilous.email@gmail.com>
2024-05-07 15:58:03 +0300

    added dark\light theme toggle

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
+// Burger menu
 let menuBtn = document.querySelector(".burger-line");
 let menu = document.querySelector(".menu__body");
 
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
+});
+
+// Dark-Light theme toggle
+const body = document.querySelector("body");
+const btnToggle = document.querySelector(".checkbox");
+
+btnToggle.addEventListener("change", () => {
+  body.classList.toggle("dark");
 });
