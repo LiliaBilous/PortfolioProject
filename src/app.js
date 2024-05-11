5bd59ff7d888834b26916dcb5c2c4805053a7480
Liliia Bilous <liliyabilous.email@gmail.com>
2024-05-11 15:43:01 +0300

    added tabMenu function&focus styles for header

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 // Burger menu
 const menuBtn = document.querySelector(".burger-line");
 const menu = document.querySelector(".menu__body");
+const headerMenu = document.querySelector(".header__menu");
 
 function toggleMenu() {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
 }
-
+function tabMenuToggle(e) {
+  if (e.key === "Enter") {
+    toggleMenu();
+  }
+}
 menuBtn.addEventListener("click", toggleMenu);
 menu.addEventListener("click", toggleMenu);
+
+headerMenu.addEventListener("keydown", tabMenuToggle);
+menu.addEventListener("keydown", tabMenuToggle);
 
 // Dark-Light theme toggle
 const body = document.querySelector("body");
 const btnToggle = document.querySelector(".checkbox");
+const themeToggle = document.querySelector(".header__theme-toggle");
 
 btnToggle.addEventListener("change", () => {
   body.classList.toggle("dark");
+});
+
+themeToggle.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" || event.key === " ") {
+    btnToggle.click();
+  }
 });
 
 // Scroll to section
